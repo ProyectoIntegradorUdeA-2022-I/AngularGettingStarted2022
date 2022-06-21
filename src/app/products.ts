c521408a0114d85d9fc537ff05affdda2c237887
Diego Herrera <daherre0@gmail.com>
2022-06-21 02:13:20 -0500

    Create the Shopping Cart Service, Create the Cart View

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,13 +10,15 @@ export const products: Product[] = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens',
+    description:
+      'A large phone with one of the best screens, and easy to enjoy all your multimedia contents.',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras',
+    description:
+      'A great phone with one of the best cameras, and easy to carry with.',
   },
   {
     id: 3,
